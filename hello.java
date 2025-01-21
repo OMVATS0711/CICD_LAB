2b135cb0e992b43c246f37af0dc2041b02b64bac
Om Vats <144024782+OMVATS0711@users.noreply.github.com>
2025-01-21 11:36:53 +0530

    Update and rename Main.java to hello.java

--- hello.java
+++ hello.java
@@ -1,4 +1,4 @@
-public class Main {
+public class hello {
     public static void main(String[] args) {
         // Print a simple message to the console
         System.out.println("Hello, World!");
